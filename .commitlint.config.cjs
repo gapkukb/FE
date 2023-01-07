ca19184fac3e3f1384da3d6fa7af8e7a594d4abf
Carlos <gapkukb@github.com>
2023-01-08 02:21:40 +0800

    feat(增加配置): 增加配置

--- .commitlint.config.cjs
+++ .commitlint.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'perf', 'refactor', 'build', 'style', 'docs', 'chore']],
-    'type-case': [0],
-  },
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'perf', 'refactor', 'build', 'style', 'docs', 'chore']],
+        'type-case': [0],
+    },
+    "config": {
+        "commitizen": {
+            "path": "@commitlint/cz-commitlint"
+        }
+    }
 };
